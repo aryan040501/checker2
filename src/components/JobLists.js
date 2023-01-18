951df47cd4b4011aa72cd2eb5288817336960f75
aryan040501 <aryan040501@gmail.com>
2023-01-19 03:35:08 +0530

    Update JobLists.js

--- src/components/JobLists.js
+++ src/components/JobLists.js
@@ -3,6 +3,7 @@ import JobCard from "./JobCard";
 import Sidebar from "./Sidebar";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import Footer from "./Footer";
 
 function JobLists() {
   const navigate = useNavigate();
@@ -22,39 +23,46 @@ function JobLists() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3">
-          <div>
-            <br />
-            <br />
-            <br />
-            <Sidebar />
-          </div>
-        </div>
-        <div className="col-md-7">
-          <div>
-            <br />
-            <div class="d-flex justify-content-center f2-bold text-dark">
-              {jobs.length} Jobs Found
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <div>
+              <br />
+              <br />
+              <br />
+              <Sidebar />
             </div>
-            {/* send datas to jobCard */}
-            <center>
-              Don't worry if you don't find a job of your liking here. <br />
-              Your Resume will be shared directly to recruiters that are not
-              present here.
-            </center>
-            {jobs.map((datas) => (
-              <div>
-                <br />
-                <JobCard data={datas} />
-                <br />
+          </div>
+          <div className="col-md-7">
+            <div>
+              <br />
+              <div class="d-flex justify-content-center f2-bold text-dark">
+                {jobs.length} Jobs Found
               </div>
-            ))}
+              {/* send datas to jobCard */}
+              <center>
+                Don't worry if you don't find a job of your liking here. <br />
+                Your Resume will be shared directly to recruiters that are not
+                present here.
+              </center>
+              {jobs.map((datas) => (
+                <div>
+                  <br />
+                  <JobCard data={datas} />
+                  <br />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <br />
+      <br />
+      <br />
+
+      <Footer />
+    </>
   );
 }
 
